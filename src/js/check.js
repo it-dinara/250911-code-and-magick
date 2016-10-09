b1b694ff1d4013fddf921b5ac6b38950e6cb9da0
Dinara <ditiviola@gmail.com>
2016-10-09 16:11:24 +0300

    исправила ошибки

--- src/js/check.js
+++ src/js/check.js
@@ -1,33 +1,33 @@
 'use strict';
 
-function getMessage(a,b) { 
-	if (typeof a=='boolean') {
-		if (a==true) {
-			return 'Я попал в ' + b;
-		} else {
-			return 'Я никуда не попал';
-		}
-	}
+function getMessage(a, b) {
+  if (typeof a === 'boolean') {
+    if (a === true) {
+      return 'Я попал в ' + b;
+    } else {
+      return 'Я никуда не попал';
+    }
+  }
 
-	if (typeof a=='number') {
-		return 'Я прыгнул на ' + a * 100 + ' сантиметров';
-	}
+  if (typeof a === 'number') {
+    return 'Я прыгнул на ' + a * 100 + ' сантиметров';
+  }
 
-	if (typeof a=='array') {
-		var numberOfSteps = 0;
-		for (var i = 0; i < a.length; i++) {
-			numberOfSteps = numberOfSteps + a[i];
-		}
-		return 'Я прошёл ' + numberOfSteps + ' шагов';
-	}
+  if (typeof a === 'array') {
+    var numberOfSteps = 0;
+    for (var i = 0; i < a.length; i++) {
+      numberOfSteps = numberOfSteps + a[i];
+    }
+    return 'Я прошёл ' + numberOfSteps + ' шагов';
+  }
 
-	if (typeof a=='array' && typeof b=='array') {
-		var distancePath = 0;
-		for (var i = 0; i < a.length; i++) {
-			distancePath = distancePath + (a[i]*b[i]);
-		};
-		return 'Я прошёл' + distancePath + 'метров';
-	}
+  if (typeof a === 'array' && typeof b === 'array') {
+    var distancePath = 0;
+    for (var i = 0; i < a.length; i++) {
+      distancePath = distancePath + (a[i]*b[i]);
+    };
+    return 'Я прошёл' + distancePath + 'метров';
+  }
 
-	return 'Переданы некорректные данные';
+  return 'Переданы некорректные данные';
 }
