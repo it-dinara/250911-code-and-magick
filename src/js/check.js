6ce9c9d1e7e3dc4fe8560e133bf397e89704e3b2
Dinara <ditiviola@gmail.com>
2016-10-09 16:31:10 +0300

    object

--- src/js/check.js
+++ src/js/check.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var getMessage = function(a, b) {
+function getMessage(a, b) {
   if (typeof a === 'boolean') {
     if (a === true) {
       return 'Я попал в ' + b;
@@ -13,7 +13,7 @@ var getMessage = function(a, b) {
     return 'Я прыгнул на ' + a * 100 + ' сантиметров';
   }
 
-  if (typeof a === 'array') {
+  if (typeof a === 'object') {
     var numberOfSteps = 0;
     for (var i = 0; i < a.length; i++) {
       numberOfSteps = numberOfSteps + a[i];
@@ -21,7 +21,7 @@ var getMessage = function(a, b) {
     return 'Я прошёл ' + numberOfSteps + ' шагов';
   }
 
-  if (typeof a === 'array' && typeof b === 'array') {
+  if (typeof a === 'object' && typeof b === 'object') {
     var distancePath = 0;
     for (var j = 0; j < a.length; j++) {
       distancePath = distancePath + (a[j] * b[j]);
